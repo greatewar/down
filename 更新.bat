d00895740d44c84d5968687d59f53c4561db08bc
greatewar <greatewar@gmail.com>
2023-05-18 23:37:24 +0800

    update

--- 更新.bat
+++ 更新.bat
@@ -1,5 +1,5 @@
-git config --global http.proxy 'socks5://127.0.0.1:10808'
-git config --global https.proxy 'socks5://127.0.0.1:10808'
+git config --global http.proxy 'socks5://127.0.0.1:10810'
+git config --global https.proxy 'socks5://127.0.0.1:10810'
 git add .
 git commit -m "update"
 git push origin master
